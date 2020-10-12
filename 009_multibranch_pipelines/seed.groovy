498741721f37f038d0a5d0d75254869fefffb11b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-10-12 16:01:01 +0200

    content(modules): add dsl for multibranch

--- 009_multibranch_pipelines/seed.groovy
+++ 009_multibranch_pipelines/seed.groovy
@@ -7,3 +7,12 @@ multibranchPipelineJob('simple') {
         }
     }
 }
+multibranchPipelineJob('simple2') {
+    branchSources {
+        git {
+            id('cicd') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/gabrielstar/jenkins_training.git')
+            includes('*')
+        }
+    }
+}
