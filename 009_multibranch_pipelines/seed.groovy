c76a149b926688289abcbf19ae1f2b02bd07ef90
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-10-12 21:22:19 +0200

    multibranch(content): added more examples

--- 009_multibranch_pipelines/seed.groovy
+++ 009_multibranch_pipelines/seed.groovy
@@ -2,7 +2,8 @@ multibranchPipelineJob('simple') {
     branchSources {
         github {
             id('cicd') // IMPORTANT: use a constant and unique identifier
-            repository('gabrielstar/jenkins_training')
+            repoOwner('gabrielstar')
+            repository('jenkins_training')
             includes('*')
         }
     }
