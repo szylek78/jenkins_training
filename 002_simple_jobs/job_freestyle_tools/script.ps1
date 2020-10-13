61521a1d326c1a8ea806c62a1ea7348481fc66e5
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-10-13 18:14:37 +0200

    refactor(refactor): change naming

--- 002_simple_jobs/job_freestyle_tools/script.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Check docker"
-docker run hello-world
-Write-Host "Check python"
-python --version
--- 002_simple_jobs/job_with_credentials/script.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "$env:creds"
-Invoke-WebRequest -URI  http://localhost:9090/jnlpJars/jenkins-cli.jar -OutFile $PWD\cli.jar
-java -jar cli.jar -auth $env:creds -s http://localhost:9090/ list-jobs
